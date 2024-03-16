8434e4460eb5f59d4363b3d61a8e5feb2e1f07a2
Amit Roy <amit.akr.roy@gmail.com>
2024-03-16 12:34:29 +0530

    Handling Events

---
+++ image.js
@@ -0,0 +1,14 @@
+window.onload = init
+function init() {
+    var images = document.getElementsByTagName("img");
+    for (var i = 0 ; i < images.length ; i ++) {
+        images[i].onclick = showAnswer;
+    }
+}
+
+function showAnswer(eventObj) {
+    var image = eventObj.target;
+    var name = image.id;
+    name = name + ".jpg";
+    image.src = "assets/imageGuess/"+name;
+}
